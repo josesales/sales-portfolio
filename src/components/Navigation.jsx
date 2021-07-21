4f2f7bcf40679baedf7838132d97feef758597de
unknown <salesbass@google.com>
2021-07-21 14:30:57 +0200

    Inplementing  navigation click handler

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { navigationItems } from '../data/navigation';
 
 const Navigation = () => {
 
+    const [isNavigationOn, setIsNavigationOn] = useState(false);
+
+    const onNavigationClick = () => {
+        setIsNavigationOn(!isNavigationOn);
+    }
+
     const navigationItemsUi = navigationItems.map((item, index) => <li key={index} className="navigation__item">
-        <a href={item.link} className="navigation__link">{item.name}</a></li>);
+        <a href={item.link} onClick={onNavigationClick} className="navigation__link">{item.name}</a></li>);
 
     return (
         <div className="navigation">
-            <input type="checkbox" id="nav-toggle" className="navigation__checkbox" />
+            <input type="checkbox" id="nav-toggle" checked={isNavigationOn} className="navigation__checkbox" />
 
-            <label htmlFor="nav-toggle" className="navigation__button">
+            <label htmlFor="nav-toggle" onClick={onNavigationClick} className="navigation__button">
                 <span className="navigation__icon"></span>
             </label>
 
