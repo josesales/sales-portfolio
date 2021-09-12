5148c9c85d640b9e452671fd68f0599952bc3408
josesales <salesbass@gmail.com>
2021-09-12 15:11:33 +0200

    Adjustment in companies section

--- src/data/companies.js
+++ src/data/companies.js
@@ -2,9 +2,9 @@ import TcsImg from '../assets/tcs.jpg'
 import HealthSolutionsImg from '../assets/health-solutions.jpg'
 import TrixlogImg from '../assets/trixlog.jpg'
 import DyadImg from '../assets/dyad.jpg';
-import BnbImg from '../assets/bnb.jpg';
+import IndraImg from '../assets/indra.jpg';
 import DataprevImg from '../assets/dataprev.jpg'
 
 
 export const companies1 = [TcsImg, HealthSolutionsImg, TrixlogImg];
-export const companies2 = [DyadImg, BnbImg, DataprevImg];
+export const companies2 = [DyadImg, IndraImg, DataprevImg];
