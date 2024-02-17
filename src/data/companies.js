fd251a30b684d4f1fd5922936938aa0e33bdc499
josesales <salesbass@gmail.com>
2024-02-17 11:42:52 +0100

    updating company array

--- src/data/companies.js
+++ src/data/companies.js
@@ -3,8 +3,8 @@ import HealthSolutionsImg from '../assets/health-solutions.jpg'
 import TrixlogImg from '../assets/trixlog.jpg'
 import DyadImg from '../assets/dyad.jpg';
 import IndraImg from '../assets/indra.jpg';
-import DataprevImg from '../assets/dataprev.jpg'
+import PwcImg from '../assets/pwc.jpg'
 
 
-export const companies1 = [TcsImg, HealthSolutionsImg, TrixlogImg];
-export const companies2 = [DyadImg, IndraImg, DataprevImg];
+export const companies1 = [PwcImg, TcsImg, HealthSolutionsImg];
+export const companies2 = [TrixlogImg, DyadImg, IndraImg];
