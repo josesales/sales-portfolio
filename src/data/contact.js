47b69ab9595bbb6f4049196e1fc9d9cb4ae82d36
josesales <salesbass@gmail.com>
2024-02-24 00:32:31 +0100

    commenting resume

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,7 +1,7 @@
 import EmailIcon from '../assets/email.svg';
 import LinkedinIcon from '../assets/linkedin.svg';
 import GitHubIcon from '../assets/github.svg';
-import ResumeIcon from '../assets/resume.svg';
+// import ResumeIcon from '../assets/resume.svg';
 
 export const contact = [
     {
@@ -19,9 +19,9 @@ export const contact = [
         link: 'https://www.linkedin.com/in/jos%C3%A9-sales-a58809135/',
         icon: LinkedinIcon,
     },
-    {
-        name: 'Resume',
-        link: 'https://drive.google.com/file/d/1KHkACBw7hRczfXaVmhYkE_zDXUqy2ZYK/view?usp=drivesdk',
-        icon: ResumeIcon,
-    },
+    // {
+    //     name: 'Resume',
+    //     link: 'https://drive.google.com/file/d/1KHkACBw7hRczfXaVmhYkE_zDXUqy2ZYK/view?usp=drivesdk',
+    //     icon: ResumeIcon,
+    // },
 ]
