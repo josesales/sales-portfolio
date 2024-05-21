0674f65d1bdf2192cb66d205a89adece9e1ef0f5
josesales <salesbass@gmail.com>
2024-05-21 04:54:22 -0300

    Updating resume link and intro description

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,27 +1,27 @@
-import EmailIcon from '../assets/email.svg';
-import LinkedinIcon from '../assets/linkedin.svg';
-import GitHubIcon from '../assets/github.svg';
-import ResumeIcon from '../assets/resume.svg';
+import EmailIcon from "../assets/email.svg";
+import LinkedinIcon from "../assets/linkedin.svg";
+import GitHubIcon from "../assets/github.svg";
+import ResumeIcon from "../assets/resume.svg";
 
 export const contact = [
-    {
-        name: 'Email',
-        link: 'mailto: contact@jose-sales.net',
-        icon: EmailIcon,
-    },
-    {
-        name: 'GitHub',
-        link: 'https://github.com/josesales',
-        icon: GitHubIcon,
-    },
-    {
-        name: 'LinkedIn',
-        link: 'https://www.linkedin.com/in/jose-sales-developer/',
-        icon: LinkedinIcon,
-    },
-    {
-        name: 'Resume',
-        link: 'https://drive.google.com/file/d/1lljJ2eKnOVbZ8Z2cL4f1MpBiWcelqN5D/view?usp=drivesdk',
-        icon: ResumeIcon,
-    },
-]
+  {
+    name: "Email",
+    link: "mailto: contact@jose-sales.net",
+    icon: EmailIcon,
+  },
+  {
+    name: "GitHub",
+    link: "https://github.com/josesales",
+    icon: GitHubIcon,
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/jose-sales-developer/",
+    icon: LinkedinIcon,
+  },
+  {
+    name: "Resume",
+    link: "https://drive.google.com/file/d/1fggxBf0AZ52XrfEXPKyMVUTcC8YKYegv/view?usp=drivesdk",
+    icon: ResumeIcon,
+  },
+];
