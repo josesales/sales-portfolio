ec8683bd30846326b18785c74d319e595793e691
Jose Sales <salesbass@gmail.com>
2024-11-19 10:33:09 -0300

    Updating resume

--- src/data/contact.js
+++ src/data/contact.js
@@ -4,24 +4,24 @@ import GitHubIcon from "../assets/github.svg";
 import ResumeIcon from "../assets/resume.svg";
 
 export const contact = [
-    {
-        name: 'Email',
-        link: 'mailto: contact@jose-sales.net',
-        icon: EmailIcon,
-    },
-    {
-        name: 'GitHub',
-        link: 'https://github.com/josesales',
-        icon: GitHubIcon,
-    },
-    {
-        name: 'LinkedIn',
-        link: 'https://www.linkedin.com/in/jose-sales-developer/',
-        icon: LinkedinIcon,
-    },
-    {
-        name: 'Resume',
-        link: 'https://drive.google.com/file/d/1vW29Pzvr9i6u9fsgrE4yX2V3RGLgrqWL/view?usp=drivesdk',
-        icon: ResumeIcon,
-    },
-]
+  {
+    name: "Email",
+    link: "mailto: contact@jose-sales.net",
+    icon: EmailIcon,
+  },
+  {
+    name: "GitHub",
+    link: "https://github.com/josesales",
+    icon: GitHubIcon,
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/jose-sales-developer/",
+    icon: LinkedinIcon,
+  },
+  {
+    name: "Resume",
+    link: "https://drive.google.com/file/d/1kk7s3OaLxx341_BzeYeUabUZvB6HZfjS/view?usp=drivesdk",
+    icon: ResumeIcon,
+  },
+];
