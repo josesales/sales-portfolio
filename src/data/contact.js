f21c31828e42ed9bebcedfba89139ea7541ed29e
Jose Sales <salesbass@gmail.com>
2025-03-14 07:17:43 -0300

    Updating cv

--- src/data/contact.js
+++ src/data/contact.js
@@ -25,7 +25,7 @@ export const contact = [
   {
     id: "contact-id-4",
     name: "Resume",
-    link: "https://drive.google.com/file/d/1rJkEKlCagJW1bMDxN3de5Bj3XzHgsliV/view?usp=drivesdk",
+    link: "https://drive.google.com/file/d/1mLHVv7CPF2jW3XOePOTFbMsK98zCgw02/view?usp=drivesdk",
     icon: ResumeIcon,
   },
 ];
