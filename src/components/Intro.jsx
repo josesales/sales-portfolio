72c0b19ea05fbe1c94a61ff90b63787d5d69423e
Jose Sales <salesbass@gmail.com>
2025-03-23 08:15:02 -0300

    Updating intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -15,7 +15,7 @@ const Intro = () => {
 
       <div className="intro__description section-margin-2 secondary-background">
         <p>
-          Hi, I'm Sales, a dedicated Full Stack Developer with 15+ years of
+          Hi, I'm Sales, a Senior Software Engineer with 15+ years of
           professional experience and a Bachelor's Degree in Computer Science.
         </p>
         <p>
